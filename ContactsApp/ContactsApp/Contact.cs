7e4fff9119a7978b59456fc4920af2103086d5e7
Третьяков Владислав Владимирович <Tretvl.work@gmail.com>
2021-03-22 17:56:21 +0700

    Изменено оформление коментариев. Добавлена проверка даты рождения больше текущей даты

--- ContactsApp/ContactsApp/Contact.cs
+++ ContactsApp/ContactsApp/Contact.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ContactsApp
 {
+    /// <summary>
+    /// Класс <see cref="Contact"/> содержит информацию о контакте
+    /// </summary>
     public class Contact
     {
         /// <summary>
-        /// Возвращает и задает имя
+        /// Содержит имя
         /// </summary>
         private string _name;
+
+        /// <summary>
+        /// Возвращает и задает имя <see cref="Name"/>
+        /// </summary>
         public string Name
         {
             get
@@ -20,22 +28,27 @@ namespace ContactsApp
             }
             set
             {
-                if (_name.Length > 50)
+                if (value.Length > 50)
                 {
                     throw new ArgumentException("Длина имени не может превышать 50 символов");
                 }
-                if (_name.Length == 0)
+                if (value.Length == 0)
                 {
                     throw new ArgumentException("Поле имени не может быть пустым");
                 }
-                _name = value;
+                TextInfo text = CultureInfo.CurrentCulture.TextInfo;
+                _name = text.ToTitleCase(value);
             }
         }
 
         /// <summary>
-        /// Возвращает и задает фамилию
+        /// Содержит фамилию
         /// </summary>
         private string _lastName;
+
+        /// <summary>
+        /// Возвращает и задает фамилию <see cref="LastName"/>
+        /// </summary>
         public string LastName
         {
             get
@@ -44,21 +57,76 @@ namespace ContactsApp
             }
             set
             {
-                if (_lastName.Length > 50)
+                if (value.Length > 50)
                 {
                     throw new ArgumentException("Длина имени не может превышать 50 символов");
                 }
-                if (_lastName.Length == 0)
+                if (value.Length == 0)
                 {
                     throw new ArgumentException("Поле имени не может быть пустым");
                 }
-                _lastName = value;
+
+                TextInfo text = CultureInfo.CurrentCulture.TextInfo;
+                _lastName = text.ToTitleCase(value);
             }
         }
+
         /// <summary>
-        /// Возвращает и задает дату рождения
+        /// Содержит email
+        /// </summary>
+        private string _email;
+
+        /// <summary>
+        /// Возвращает и задает email <see cref="Email"/>
+        /// </summary>
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                if (value.Length > 50)
+                {
+                    throw new ArgumentException("Длинна email не может " +
+                                                "превышать 50 симоволов");
+                }
+                _email = value;
+            }
+        }
+
+        /// <summary>
+        /// Содержит vkID
+        /// </summary>
+        private string _vkID;
+
+        ///<summary>
+        ///Возвращает и задает ID вконтакте <see cref="VkID"/>
+        /// </summary>
+        public string VkID
+        {
+            get
+            {
+                return _vkID;
+            }
+            set
+            {
+                if (value.Length > 15)
+                {
+                    throw new ArgumentException("Длинна vkID не может превышать 15 символов");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Содержит дату рождения
         /// </summary>
         private DateTime _birthday;
+
+        /// <summary>
+        /// Возвращает и задает дату рождения <see cref="Birthday"/>
+        /// </summary>
         public DateTime Birthday
         {
             get
@@ -67,6 +135,10 @@ namespace ContactsApp
             }
             set
             {
+                if (DateTime.Today > _birthday)
+                {
+                    throw new ArgumentException("Дата рождения не может быть позже текущей даты");
+                }
                 if (_birthday.Year < 1900)
                 {
                     throw new ArgumentException("Дата рождения не может быть раньше 1900 года");
--- ContactsApp/ContactsApp/PhoneNumber.cs
+++ ContactsApp/ContactsApp/PhoneNumber.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace ContactsApp
 {
+    /// <summary>
+    /// Class <see cref="PhoneNumber"> contains the telephone number of the person
+    /// </summary>
     class PhoneNumber
     {
-        
+        /// <summary>
+        /// Содержит номер телефона
+        /// </summary>
+        private long _phoneNumber;
     }
 }
