226b88278bea6bde3dcb74a51fd0d6546520032a
Saxonic <Saxonic@users.noreply.github.com>
2021-07-03 15:52:37 +0700

    Lab 4. Изменение структуры юнит тестов в PhoneNumber Добавление коментариев

--- ContactsApp/ContactsApp.UnitTests/PhoneNumberTests.cs
+++ ContactsApp/ContactsApp.UnitTests/PhoneNumberTests.cs
@@ -12,40 +12,56 @@ namespace ContactsApp.UnitTests
 		}
 
 		[Test(Description = "Positive test of the Number getter")]
-		public void TestNumberGet_CorrectValue()
+		public void NumberGet_ReturnCorrectValue()
 		{
+			//setup
 			var expected = 78005553535;
-
-			var phoneNumber = CreateClearPhoneNumber();
+            var phoneNumber = CreateClearPhoneNumber();
 			phoneNumber.Number = expected;
-
+			
+            //act
 			var actual = phoneNumber.Number;
 
+			//assert
 			Assert.AreEqual(expected, actual,
 				"Getter Number returns incorrect value");
 		}
 
-		[TestCase(88005553535, "An exception should occur if the phone Number starts at 7",
-			TestName = "Assignment of the Number starting not with 7")]
-		[TestCase(780055535353, "An exception should occur if the phone Number has 11 numbers",
-			TestName = "Assignment of the Number has not 11 numbers")]
-		public void TestNumberSet_ArgumentException(long wrongNumber, string message)
-		{
-			var phoneNumber = CreateClearPhoneNumber();
-			Assert.Throws<ArgumentException>(
-				() => { phoneNumber.Number = wrongNumber; },
-				message);
+		[Test(Description = "Negative number setter test")]
+		public void NumberSet_StartNotFromSeven_ArgumentException()
+        {
+            //setup
+            var phoneNumber = CreateClearPhoneNumber();
+            var wrongNumber = 21234567890;
+
+			//assert
+            Assert.Throws<ArgumentException>(
+				() => { phoneNumber.Number = wrongNumber; });
 		}
 
-		[Test(Description = "Positive test of the Number setter")]
-		public void TestNumberSet_CorrectValue()
-		{
-			var number = 78005553535;
 
-			var phoneNumber = CreateClearPhoneNumber();
-			Assert.DoesNotThrow(
-				() => { phoneNumber.Number = number; },
-				"Positive test of the Number setter not passed");
+        [Test(Description = "Negative number setter test")]
+        public void NumberSet_IncorrectLength_ArgumentException()
+        {
+            //setup
+            var phoneNumber = CreateClearPhoneNumber();
+            var wrongNumber = 2123456;
+
+            //assert
+            Assert.Throws<ArgumentException>(
+                () => { phoneNumber.Number = wrongNumber; });
+        }
+
+		[Test(Description = "Positive test of the Number setter")]
+        public void NumberSet_CorrectValue()
+		{
+			//setup
+			var number = 78005553535;
+            var phoneNumber = CreateClearPhoneNumber();
+
+			//assert
+            Assert.DoesNotThrow(
+				() => { phoneNumber.Number = number; });
 		}
 
 		[Test(Description = "Positive test of the Constructor")]
@@ -53,8 +69,7 @@ namespace ContactsApp.UnitTests
 		{
 			var number = 78005553535;
 			Assert.DoesNotThrow(
-				() => { new PhoneNumber(number); },
-				"Constructor is not passed");
+				() => { new PhoneNumber(number); });
 		}
 	}
 }
